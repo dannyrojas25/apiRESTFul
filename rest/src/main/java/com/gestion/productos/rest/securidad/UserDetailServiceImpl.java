5f493ce3f5e4a8663c236e0fdd831a082f4e3cc6
dannyrojas25 <ingalexisrojas2@gmail.com>
2023-09-19 18:34:24 -0500

    Add files via upload

---
+++ rest/src/main/java/com/gestion/productos/rest/securidad/UserDetailServiceImpl.java
@@ -0,0 +1,25 @@
+package com.gestion.productos.rest.securidad;
+
+import com.gestion.productos.rest.modelo.Usuario;
+import com.gestion.productos.rest.repositorio.UsuarioRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepositorio usuarioRepositorio;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        Usuario usuario =usuarioRepositorio
+                .findOneByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("El usuario con email " + email + "no existe."));
+
+        return new UserDetailsImpl(usuario);
+    }
+}
